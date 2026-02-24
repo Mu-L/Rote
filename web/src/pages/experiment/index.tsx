41d899a3d86e97e11846cc6c0fcb498d3f3509e6
Rabithua <rabithua@gmail.com>
2026-02-24 20:04:16 +0800

    refactor: unify navbar and page layout logic for admin and experiment pages

--- web/src/pages/experiment/index.tsx
+++ web/src/pages/experiment/index.tsx
@@ -3,6 +3,7 @@ import ExportData from '@/components/experiment/exportData';
 import ImportData from '@/components/experiment/importData';
 import ServiceWorker from '@/components/experiment/serviceWorker';
 import NavBar from '@/components/layout/navBar';
+import ContainerWithSideBar from '@/layout/ContainerWithSideBar';
 import { Snail } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 
@@ -11,7 +12,7 @@ export default function ExperimentPage() {
     keyPrefix: 'pages.experiment',
   });
   return (
-    <div className="noScrollBar relative flex-1 divide-y overflow-x-hidden overflow-y-visible pb-20">
+    <ContainerWithSideBar>
       <NavBar title={`${t('title')}`} icon={<Snail className="size-6" />} />
       <div className="flex w-full flex-col divide-y">
         <div className="px-4 py-3 font-thin">{t('description')}</div>
@@ -22,6 +23,6 @@ export default function ExperimentPage() {
           <EveCat />
         </div>
       </div>
-    </div>
+    </ContainerWithSideBar>
   );
 }
