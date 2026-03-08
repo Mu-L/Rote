e3dc8470cf38ea46a0f23324e1d6fe3496048aea
Rabithua <rabithua@gmail.com>
2026-03-08 12:06:05 +0800

    style: adjust dashboard sidebar responsive breakpoints from xl to lg

--- web/src/layout/dashboard/index.tsx
+++ web/src/layout/dashboard/index.tsx
@@ -141,7 +141,7 @@ function LayoutDashboard() {
           } `}
         >
           {icon.svg}
-          <div className="hidden shrink-0 tracking-widest xl:block">
+          <div className="hidden shrink-0 tracking-widest lg:block">
             {t(`leftNavBar.${icon.name}`)}
           </div>
         </div>
@@ -157,7 +157,7 @@ function LayoutDashboard() {
         onClick={() => setIsLogoutDialogOpen(true)}
       >
         {icon.svg}
-        <div className="hidden shrink-0 tracking-widest xl:block">
+        <div className="hidden shrink-0 tracking-widest lg:block">
           {t(`leftNavBar.${icon.name}`)}
         </div>
       </div>
@@ -168,7 +168,7 @@ function LayoutDashboard() {
     <>
       <div className="bg-background text-primary mx-auto w-full max-w-6xl">
         <div className="mx-auto flex w-dvw max-w-[1440px] font-sans sm:divide-x xl:w-[90%]">
-          <div className="bg-background/90 text-primary fixed bottom-0 z-10 flex w-full shrink-0 flex-row items-start justify-around px-1 py-2 pb-6 backdrop-blur-xl sm:sticky sm:top-0 sm:h-dvh sm:w-fit sm:flex-col sm:justify-center sm:gap-4 sm:px-2 xl:w-[200px] xl:px-4">
+          <div className="bg-background/90 text-primary fixed bottom-0 z-10 flex w-full shrink-0 flex-row items-start justify-around px-1 py-2 pb-6 backdrop-blur-xl sm:sticky sm:top-0 sm:h-dvh sm:w-fit sm:flex-col sm:justify-center sm:gap-4 sm:px-2 lg:w-[200px] lg:px-4">
             {tokenValid
               ? userTabs.map((icon) => IconRenderItem(icon))
               : authReady
